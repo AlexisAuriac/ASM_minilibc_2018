bc9f0ec37dbc5d3c85dcc1946efaf00025904dd6
alexis <alexis.auriac@epitech.eu>
2019-02-14 15:58:30 +0100

    Function done

--- src/memset.asm
+++ src/memset.asm
@@ -1,17 +1,40 @@
 BITS 64
 
+section .bss
+
+s       resq    1
+n       resq    1
+
 section .text:
 
-global  my_memset:function
+global memset:function
 
 ; rdi -> void *s
 ; rsi -> int c
 ; rdx -> size_t n
-my_memset:
+memset:
     push rbp
     mov rbp, rsp
 
+    mov [s], rdi
+    mov [n], rdx
+
+    mov rcx, 0
+
+L1:
+    cmp rcx, [n]
+    je END
+
+    xor rax, rax
+    mov rax, [s]
+    add rax, rcx ; rax now contains address to current byte
+
+    mov byte [rax], sil
+
+    inc rcx
+    jmp L1
+
 END:
-    mov rax, rdi
+    mov rax, [s]
     leave
     ret
