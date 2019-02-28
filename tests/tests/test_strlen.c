60071afbc5197b174476a40473d66fe5c67b3335
alexis <alexis.auriac@epitech.eu>
2019-02-28 18:09:13 +0100

    Started setting up

---
+++ tests/tests/test_strlen.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2017
+** minilibc
+** File description:
+** Tests strlen.
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <criterion/criterion.h>
+
+Test(test_strlen, base)
+{
+    // cr_assert_eq(strlen("hello"), 5);
+    // cr_assert(1);
+}
