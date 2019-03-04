8947242f0fe781a33488e138e99aea41d95c5d65
alexis <alexis.auriac@epitech.eu>
2019-03-05 00:39:09 +0100

    Tests for strncmp done

--- tests/tests/test_strcmp.c
+++ tests/tests/test_strcmp.c
@@ -15,6 +15,7 @@ Test(test_strcmp, base)
     void *lib = dlopen(LIB, RTLD_LAZY);
     int (*my_strcmp)(const char *, const char *) = dlsym(lib, "strcmp");
 
+    cr_assert_neq(my_strcmp, NULL);
     cr_assert_eq(my_strcmp("a", "a"), 0);
     cr_assert_eq(my_strcmp("", ""), 0);
     cr_assert_eq(my_strcmp("abcdef", "abcdef"), 0);
@@ -26,4 +27,5 @@ Test(test_strcmp, base)
     cr_assert_gt(my_strcmp("abcda", "abcd"), 0);
     cr_assert_lt(my_strcmp("", " "), 0);
     cr_assert_gt(my_strcmp(" ", ""), 0);
+    dlclose(lib);
 }
