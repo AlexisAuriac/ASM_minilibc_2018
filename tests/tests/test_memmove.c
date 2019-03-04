fe5c9ed18fea3a0c5aae2f9b71950b4e376caa55
alexis <alexis.auriac@epitech.eu>
2019-03-04 19:57:07 +0100

    Tests for memmove done

--- tests/tests/test_memmove.c
+++ tests/tests/test_memmove.c
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2017
 ** minilibc
 ** File description:
-** Tests memcpy.
+** Tests memmove.
 */
 
 #include <stdlib.h>
@@ -10,21 +10,22 @@
 #include <dlfcn.h>
 #include <criterion/criterion.h>
 
-Test(test_memcpy, base)
+Test(test_memmove, base)
 {
     void *lib = dlopen(LIB, RTLD_LAZY);
-    char *(*my_memcpy)(void *, void *, size_t) = dlsym(lib, "memcpy");
+    char *(*my_memmove)(void *, void *, size_t) = dlsym(lib, "memmove");
     char str[100];
 
     memset(str, 'z', 100);
-    cr_assert_neq(my_memcpy, NULL);
-    my_memcpy(str, "abcdef", 6);
+    cr_assert_neq(my_memmove, NULL);
+    my_memmove(str, "abcdef", 6);
     cr_assert_eq(strncmp(str, "abcdefz", 7), 0);
     str[6] = '\0';
-    my_memcpy(str, "def", 4);
+    my_memmove(str, "def", 4);
     cr_assert_str_eq(str, "def");
-    my_memcpy(str, "abcde", 6);
+    my_memmove(str, "abcde", 6);
     cr_assert_str_eq(str, "abcde");
-    cr_assert_str_eq(my_memcpy(str + 1, str, 3), "aaae");
+    cr_assert_str_eq(my_memmove(str + 1, str, 3), "abce");
+    dlclose(lib);
     dlclose(lib);
 }
