5c63b480d5d1fa5edc458e9e169b118e1c0e5c55
alexis <alexis.auriac@epitech.eu>
2019-03-04 18:40:34 +0100

    Tests for memcpy done

---
+++ tests/tests/test_memcpy.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2017
+** minilibc
+** File description:
+** Tests memcpy.
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <dlfcn.h>
+#include <criterion/criterion.h>
+
+Test(test_memcpy, base)
+{
+    void *lib = dlopen(LIB, RTLD_LAZY);
+    char *(*my_memcpy)(void *, void *, size_t) = dlsym(lib, "memcpy");
+    char str[100];
+
+    memset(str, 'z', 100);
+    cr_assert_neq(my_memcpy, NULL);
+    my_memcpy(str, "abcdef", 6);
+    cr_assert_eq(strncmp(str, "abcdefz", 7), 0);
+    str[6] = '\0';
+    my_memcpy(str, "def", 4);
+    cr_assert_str_eq(str, "def");
+    dlclose(lib);
+}
--- tests/tests/test_rindex.c
+++ tests/tests/test_rindex.c
@@ -14,12 +14,13 @@ Test(test_rindex, base)
 {
     void *lib = dlopen(LIB, RTLD_LAZY);
     char *(*my_rindex)(const char *, int) = dlsym(lib, "rindex");
-    const char *str = "Hello, World!";
+    const char *str = strdup("Hello, World!");
 
     cr_assert_neq(my_rindex, NULL);
     cr_assert_eq(my_rindex(str, 'l'), str + 10);
     cr_assert_eq(my_rindex(str, 'H'), str);
     cr_assert_eq(my_rindex(str, '!'), str + 12);
     cr_assert_eq(my_rindex(str, 'z'), NULL);
+    free(str);
     dlclose(lib);
 }
