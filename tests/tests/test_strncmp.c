7bca10b8d0b9b7d11aeefa9a3869d5e0988e07a5
alexis <alexis.auriac@epitech.eu>
2019-03-05 01:40:50 +0100

    Corrected some coding style errors in tests

--- tests/tests/test_strncmp.c
+++ tests/tests/test_strncmp.c
@@ -18,7 +18,6 @@ Test(test_strncmp, base)
 
     cr_assert_neq(my_strncmp, NULL);
     cr_assert_eq(my_strncmp("", "", 99), 0);
-    cr_assert_eq(my_strncmp("a", "a", 99), 0);
     cr_assert_eq(my_strncmp("abc", "abc", 99), 0);
     cr_assert_gt(my_strncmp("abcd", "abc", 99), 0);
     cr_assert_lt(my_strncmp("abc", "abcd", 99), 0);
@@ -30,7 +29,7 @@ Test(test_strncmp, base)
     cr_assert_eq(my_strncmp("abce", "abc", 3), 0);
     cr_assert_lt(my_strncmp("abcd", "abce", 4), 0);
     cr_assert_eq(my_strncmp("abc", "def", 0), 0);
-    cr_assert_gt(my_strncmp("abc", "", (size_t)-1), 0);
-    cr_assert_eq(my_strncmp("abc", "abc", (size_t)-2), 0);
+    cr_assert_gt(my_strncmp("abc", "", (size_t) -1), 0);
+    cr_assert_eq(my_strncmp("abc", "abc", (size_t) -2), 0);
     dlclose(lib);
 }
